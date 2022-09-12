47e60d8cbe67ab1a359300b392ff68b025791bf9
Ram Subramaniam <Ram.Subramaniam@outlook.com>
2022-09-12 08:35:19 -0400

    New Changes

--- Datainsert.py
+++ Datainsert.py
@@ -3,7 +3,7 @@ import pyodbc
 # insert data from csv file into dataframe.
 # working directory for csv file: type "pwd" in Azure Data Studio or Linux
 # working directory in Windows c:\users\username
-df = read_csv("c:\\user\\username\department.csv")
+
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
